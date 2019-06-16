1aadb77051bb02b91b16462fe354a33eea350e1d
Hacker719 <neel.redkar@gmail.com>
2019-06-16 08:55:32 -0700

    add event

--- cards/psswd.js
+++ cards/psswd.js
@@ -8,3 +8,8 @@ function pass(x,fileman) {
         alert("Wrong! Please try again or check the url");
     }
 }
+window.addEventListener("keydown",function(a) {
+    if (a.code == "Enter") {
+        pass('baba01','cards/8239u8ykmscauzh89y31gv123bdnas.html')
+    }
+});
